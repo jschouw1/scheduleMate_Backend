07dfe2f2f0fa476d4f69601ea1a07a5c38cca10c
Joost Schouwstra <63044232+jschouw1@users.noreply.github.com>
2020-06-15 11:55:12 +0200

    Initial commit

---
+++ src/test/java/com/jschouw/scheduleMate/ScheduleMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jschouw.scheduleMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
